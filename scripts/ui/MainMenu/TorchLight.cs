3fbeba7c634019bdce1d9238a3bbfdd429dd1676
bamoebin <farrasaer@gmail.com>
2026-02-03 18:27:32 +0700

    MainMenu beta

--- scripts/ui/MainMenu/TorchLight.cs
+++ scripts/ui/MainMenu/TorchLight.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class TorchLight : PointLight2D
 {
+	// If set, the light will follow this node's position (e.g. your Background).
+	[Export(PropertyHint.NodePathValidTypes, "Node2D")] public NodePath FollowTargetPath { get; set; }
+
 	
 	[Export] public float MinEnergy { get; set; } = 0.9f;
 	[Export] public float MaxEnergy { get; set; } = 1.0f;
@@ -10,12 +13,26 @@ public partial class TorchLight : PointLight2D
 
 	private bool _running;
 	private Vector2 _baseScale;
+	private Node2D _followTarget;
 
 	public override void _Ready()
 	{
 		_baseScale = Scale;
+		if (FollowTargetPath != null && !FollowTargetPath.IsEmpty)
+		{
+			_followTarget = GetNodeOrNull<Node2D>(FollowTargetPath);
+		}
+
 		_running = true;
 		_ = FlickerLoopAsync();
+	}
+
+	public override void _Process(double delta)
+	{
+		if (_followTarget != null)
+		{
+			GlobalPosition = _followTarget.GlobalPosition;
+		}
 	}
 
 	public override void _ExitTree()
